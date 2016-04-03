cbd9aa9b725525cbd205a8ae19c25d3553ca50b5
Jesse Wang <jesse.wang19@gmail.com>
2016-04-03 11:52:40 -0400

    Initial Commit

---
+++ image-resizer.js
@@ -0,0 +1,66 @@
+/**************************/
+/*  Back to Image Resizer */
+/**************************/
+
+/*
+  Return a string that will display the resize text based on the pixels parameter
+  ie:
+  resizeText(100) => "Resize to 100px"
+  resizeText(200) => "Resize to 200px"
+  resizeText(300) => "Resize to 300px"
+  resizeText(400) => "Resize to 400px"
+*/
+function resizeText(pixels) {
+
+}
+
+/*
+  Return a block of HTML code that creates a button using resizeText()
+  ie:
+  createButton(100) => "<button id='button100'>Resize to 100px</button>")
+  createButton(200) => "<button id='button200'>Resize to 200px</button>")
+  createButton(300) => "<button id='button300'>Resize to 300px</button>")
+  createButton(400) => "<button id='button400'>Resize to 400px</button>")
+*/
+function createButton(pixels) {
+
+}
+
+/*
+  Return the value multiplied by 100
+  ie:
+  calculateImageSize(1) => 100
+  calculateImageSize(2) => 200
+  calculateImageSize(3) => 300
+  calculateImageSize(4) => 400
+*/
+function calculateImageSize(value) {
+
+}
+
+/*
+  Append a button to the #buttons div using createButton()
+  Then add a click event on the #button + size
+  ie:
+  if size is 100 => $(#button100)
+  if size is 200 => $(#button200)
+
+  In the click event handler, change the width of the img to the specified size
+*/
+function addButton(size) {
+
+}
+
+/*
+  Create a for-loop that starts at 1 and ends at 4 inclusively
+  Inside the for-loop use calculateImageSize and set it to a variable
+  Next call the addButton function and use the variable obtained previously as its' argument
+*/
+function runForLoop() {
+
+}
+
+/*
+  Starts the code. Do not change this!
+*/
+runForLoop();
